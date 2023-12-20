a88b1d1831dfe8dd2e884151821cb136fd040d5e
Josh Breytspraak <133163031+JBreyt24@users.noreply.github.com>
2023-12-20 11:41:38 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Deploy Python Project via AWS</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
